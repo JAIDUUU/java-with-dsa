a8ce08c129e1b085fb19543d16875247eeb29a25
JAIDUUU <jaidzxxd@gmail.com>
2026-09-18 19:31:04 +0530

    1

---
+++ JAVA_DSA/src/operator.java
@@ -0,0 +1,96 @@
+public class operator {
+    public static void main(String[] args) {
+//        int solvedThisWeek =25;
+//        int solvedLastWeek=15;
+//
+//        int total =solvedLastWeek + solvedThisWeek ;
+//        int difference=solvedThisWeek - solvedLastWeek;
+//        int projected=solvedThisWeek * 4;
+//        int average=solvedThisWeek / 7;
+//        int remainder=solvedThisWeek % 7;
+//
+//        System.out.println(total);
+//        System.out.println(difference);
+//        System.out.println(projected);
+//        System.out.println(average);
+//        System.out.println(remainder);
+
+
+
+//         Assignment operator
+//        int ratingPoints=100;
+//
+//        ratingPoints+=20;
+//        System.out.println(ratingPoints);
+//        ratingPoints-=10;
+//        System.out.println(ratingPoints);
+//        ratingPoints*=4;
+//        System.out.println(ratingPoints);
+//        ratingPoints/=4;
+//        System.out.println(ratingPoints);
+//        ratingPoints%=30;
+//        System.out.println(ratingPoints);
+
+
+        // relational operator
+//        int currentStreak = 40;
+//        int targetStreak = 90;
+//
+//        System.out.println(currentStreak == targetStreak);
+//        System.out.println(currentStreak != targetStreak);
+//        System.out.println(currentStreak > targetStreak);
+//        System.out.println(currentStreak < targetStreak);
+//        System.out.println(currentStreak >= targetStreak);
+//        System.out.println(currentStreak <= targetStreak);
+//
+
+
+
+
+        //Logical operator
+
+//        boolean completedDSA=true;
+//        boolean completedCORE=false;
+//
+//        System.out.println(completedDSA && completedCORE);
+//        System.out.println(completedDSA || completedCORE);
+//        System.out.println(!completedCORE);
+//
+
+        //bitwise operation
+//
+//        int x=6;
+//        int y=3;
+//
+//        System.out.println(x & y);
+//        System.out.println(x | y);
+//        System.out.println(x ^ y);
+//        System.out.println(~x);
+//        System.out.println(x <<1);
+//        System.out.println(x>>1);
+
+
+        //instanceof Operator
+//
+//        String name="zaid";
+//        System.out.println(name instanceof String);
+
+
+
+       // order
+//        1. ()
+//        2. *, /, %
+//        3. +, -
+//        4. <, >, <=, >=
+//        5. ==, !=
+//        6. &&
+//        7. ||
+//        8. ?:
+//        9. =, +=, -=, *=, /=
+
+    }
+
+}
+
+
+
