8950a90a7a442ac8cb4c7ccceab500da366ebc16
JAIDUUU <jaidzxxd@gmail.com>
2026-09-19 22:35:25 +0530

    java pattern problem
    only 7-8 qustion solved

---
+++ JAVA_DSA/src/pattern_problem.java
@@ -0,0 +1,119 @@
+import java.util.Scanner;
+import java.util.SortedMap;
+
+public class pattern_problem {
+    static void main(String[] args) {
+
+        //Pattern 1: Solid Square Pattern
+
+//        int n=4;
+//        for (int row =1;row<=n;row++){
+//            for (int coloumn=1;coloumn<=n;coloumn++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+
+        //Hollow Square Pattern
+        Scanner sc=new Scanner(System.in);
+//        System.out.println("enter the no for hollow square ");
+//        int n=sc.nextInt();
+//
+//        for(int row=1;row<=n;row++){
+//            for (int column=1;column<=n;column++){
+//                if (row==1 || row==n){
+//                    System.out.print("* ");
+//                }
+//                else {
+//                   if (column==1 ){
+//                       System.out.print("* ");
+//                   } else if (column==n) {
+//                       System.out.print("* ");
+//                   } else {
+//                       System.out.print("  ");
+//                   }
+//                }
+//            }
+//            System.out.println();
+//            sc.close();
+//        }
+
+
+
+        //Pattern 3: Solid Right-Angle Triangle Pattern
+//        System.out.println("enter no for triangle");
+//        int n =sc.nextInt();
+//
+//        for(int row=1;row<=n;row++){
+//            for (int column=1;column<=row;column++)
+//                System.out.print("* ");
+//            System.out.println();
+//            sc.close();
+//        }
+
+
+
+        // Pattern 4: Inverted Right-Angle Triangle Pattern
+//        System.out.println("enter the no to triangle");
+//        int n =sc.nextInt();
+//
+//        for (int row =1;row<=n;row++){
+//            for (int column=1;column<=n-row;column++){
+//                System.out.print("* ");
+//
+//            }
+//            System.out.println();
+//            sc.close();
+//        }
+
+
+
+        //Pattern 5: Solid Pyramid Pattern
+//        System.out.println("enter the pyramid number");
+//        int n =sc.nextInt();
+//        for (int row =1;row<=n;row++){
+//            for (int space=1;space<=n-row;space++){
+//                System.out.print("  ");
+//            }
+//            for (int star=1;star<=2*row-1;star++)
+//                System.out.print("* ");
+//            System.out.println();
+//        }
+
+
+
+        //Hollow Right-Angle Triangle Pattern
+//        System.out.println("enter the hollow triangle no");
+//        int n= sc.nextInt();
+//
+//        for(int row=1;row<=n;row++){
+//            if (row==1  || row==2 || row==n){
+//                for (int column=1;column<=row;column++){
+//                    System.out.print("* ");
+//                }
+//
+//            }
+//            else {
+//                System.out.print("* ");
+//                for (int space=1;space<=row-2;space++){
+//                    System.out.print("  ");
+//                }
+//                System.out.print("* ");
+//
+//            }
+//            System.out.println();
+//        }
+
+
+
+
+
+
+    }
+
+
+}
+
+
+
