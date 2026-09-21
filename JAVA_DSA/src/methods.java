e9c1718934f9425a19cd80009acb9a9d5e88d815
JAIDUUU <jaidzxxd@gmail.com>
2026-09-21 21:18:32 +0530

    learn methods

---
+++ JAVA_DSA/src/methods.java
@@ -0,0 +1,129 @@
+import java.awt.image.BandedSampleModel;
+
+public class methods {
+    public static void main (String[] args){
+////        printTable(5);
+////        printSum();
+////        printSum(10,20);
+////        main();
+//
+////        int ans1=add(1,2);
+////        int ans2=add(1,2,3);
+////        float ans3=add(10,2.0f);
+////        System.out.println("ans1 "+ans1);
+////        System.out.println("ans2 "+ans2);
+////        System.out.println("ans3 "+ans3);
+//
+////        int a = 10;
+////        change(a);
+////        System.out.println("Outside: " + a);
+    }
+//
+
+
+//     static void  printTable(int i){
+//        for(i=1;i<=10;i++){
+//            System.out.println(2*i);
+//        }
+//    }
+//     static void printSum(){
+//        int a=1 ;
+//        int b=2;
+//        System.out.println(a+b);
+//    }
+//     static void printSum(int a,int b){
+//        System.out.println(a+b);
+//    }
+
+    //Void vs Non-void Return Type
+//    static void multiply(int a ,int b){
+//        System.out.println(a*b);
+//    }
+//    static void checkage(int age){
+//        if (age>=18){
+//            System.out.println("AGE is"+age);
+//        }
+//    }
+        //    non-void
+//    static int add(int p , int q){
+//        int sum = p+q;
+//        return sum;
+//    }
+
+
+    //Execution Flow and Arguments
+//    static void main(){
+//        System.out.println("hello");
+//        solve();
+//        System.out.println("byy");
+//    }
+//    static void solve(){
+//        System.out.println("this is your no ");
+//        int answ=addd(1,2);
+//        System.out.println(answ);
+//        System.out.println("by");
+//
+//
+//    }
+//    static int addd(int x ,int y){
+//        System.out.println("kiya hua");
+//        int sum = x+y;
+//        return sum;
+//
+//    }
+
+    //Method Overloading
+    //Can two methods have the same name? Yes, but their signatures must be different.
+    //Number of parameters
+    //Type of parameters
+
+
+        //2 numbers
+//    static int add(int p, int q){
+//        int sum = p+q;
+//        return sum;
+//    }
+//    // 3 numbers bt sanme name , diffrent parameter
+//    static int add(int p, int q, int r){
+//        int ans = p+q+r;
+//        return ans;
+//    }
+
+    // dikkat bas yaha dega X yei nhi kerna
+//    static int add(int p, int q, ){
+//        int ans = p+q+r;
+//        return ans;
+//    }
+    // yei ker sktei hn
+//    static float add(int p, float q) {
+//        float ans = p + q;
+//        return ans;
+//    }
+
+
+    //Call by Value
+//    static void change(int x) {
+//        x = 100;
+//        System.out.println("Inside: " + x);
+//    }
+
+
+    //Variable Scoping
+//static void printsum(int a,int b){
+//    int sum =a+b;  // 'sum' only exists inside this method
+//    System.out.println(sum);
+//}
+//public static void main(String[] args){
+//    int x=5;   // x is only exists inside main
+//    printsum(x,5);
+//    System.out.println(sum); // error 'sum' does not exist
+//}
+
+
+
+
+
+
+
+
+}
